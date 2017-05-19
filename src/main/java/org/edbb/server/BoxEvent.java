c48c9e2e07b841265f49429ca8c19c9eafdc0ddd
inb <inorton@gmail.com>
2017-05-19 20:51:31 +0100

    rename packages to hits

--- src/main/java/org/edbb/server/BoxEvent.java
+++
@@ -1,9 +0,0 @@
-package org.edbb.server;
-
-/**
- * Created by inb on 30/04/2017.
- */
-public class BoxEvent {
-    public String eventType;
-    public int eventLocation;
-}
--- src/main/java/org/edbb/server/LocationReport.java
+++
@@ -1,9 +0,0 @@
-package org.edbb.server;
-
-/**
- * Safety report on a location
- */
-public class LocationReport {
-    public String advice;
-    public float riskFactor;
-}
